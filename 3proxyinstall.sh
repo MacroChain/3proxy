4ceb039cae2623d9f43bbddfdf58f9733950a6aa
ナム <namnhfreelancer@gmail.com>
2021-04-25 16:59:58 +0900

    Update 3proxyinstall.sh

--- 3proxyinstall.sh
+++ 3proxyinstall.sh
@@ -1,5 +1,4 @@
 version=0.8.13
-apt-get update
 apt-get install gcc make git -y
 wget --no-check-certificate -O 3proxy-${version}.tar.gz https://github.com/z3APA3A/3proxy/archive/${version}.tar.gz
 tar xzf 3proxy-${version}.tar.gz
